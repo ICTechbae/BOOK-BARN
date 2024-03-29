70789837e08d76a55f9f3f33d63483e7d6a840f3
IWUOHA CHIOMA <iwuohachioma30@gmail.com>
2024-03-29 16:00:21 +0100

    Update footer.php

--- project/footer.php
+++ project/footer.php
@@ -36,6 +36,6 @@
 
    </div>
 
-   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>mr. web designer</span> </p>
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Iwuoha Chioma</span> </p>
 
 </section>
